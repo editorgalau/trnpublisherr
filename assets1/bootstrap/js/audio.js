30a70d0da526cc5825278cfd6b7cbe4fef0be8b3
editorgalau <97397607+editorgalau@users.noreply.github.com>
2022-01-26 18:13:09 +0700

    Add files via upload

---
+++ assets1/bootstrap/js/audio.js
@@ -0,0 +1,8 @@
+<script>
+		var a = document.getElementById('tampil');
+		function putarmusic(){
+		a.style.display = "none";
+		var audio = new Audio('adstxt/public/privacypolicy/play.mp3');
+		audio.play();
+		}
+</script>
